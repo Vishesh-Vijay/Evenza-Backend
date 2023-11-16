a141bb4993ba25cf4b0f1ef6bf62f995f5291cb4
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-11-16 13:28:53 +0530

    test

--- routes/events.js
+++ routes/events.js
@@ -5,12 +5,12 @@ import {
     getAllEvents,
     getEvent,
     deleteEvent,
+    createEvent
 } from "../controllers/events.js";
 
-// Routes for Events
 router.get("/", getAllEvents);
 router.get("/:eventId", getEvent);
 router.delete("/:eventId", deleteEvent);
-
+router.post("/create", createEvent)
 
 export default router;
